1bb7a6948e0d13a98b823038c35df9ebe248a089
subikstha <subsatch@gmail.com>
2026-06-17 16:28:56 +0545

    feat: added aliases for verbose eventlisteners and queryselectors

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // It is better to wait for the event for manipulation
 window.addEventListener("DOMContentLoaded", () => {
-    let nav = document.querySelector("nav")
+    let nav = $("nav")
     console.log(nav)
+
+    nav.on('click', () => {
+        alert('Hey you just clicked on the nav')
+    })
 })
+
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
+HTMLElement.prototype.off = (a, b) => this.removeEventListener(a, b);
+HTMLElement.prototype.$ = (s) => this.querySelector(s);
+HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
