db982feca94e1c39b5be06536a46f0acbd3f5364
subikstha <subsatch@gmail.com>
2026-06-18 16:56:34 +0545

    feat: created a proxiedStore

--- services/Store.js
+++ services/Store.js
@@ -3,4 +3,19 @@ const Store = {
     cart:[]
 }
 
-export default Store
+const proxiedStore = new Proxy(Store, {
+    set(target, property, value) {
+        target[property] = value
+        if(property === "menu") {
+            // Announce globally that the menu has changed
+            window.dispatchEvent(new Event("appmenuchange"))
+        }
+        if(property === "cart") {
+            window.dispatchEvent(new Event("appcartchange"))
+        }
+        // Set should return either true or false to indicate that you accepted the set or not
+        return true // Accept the setter
+    }
+})
+
+export default proxiedStore
